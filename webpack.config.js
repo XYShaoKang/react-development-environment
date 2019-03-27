f33d575802d6607aa140575f6e338209042ea79a
XYShaoKang <zengwenjie03@gmail.com>
2019-03-27 15:21:34 +0800

    chore: 调整 webpack 根据不同的环境设置使用不同的配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'React Demo',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: { loader: 'babel-loader' },
-      },
+module.exports = env => {
+  const dev = env && env.dev
+  const prod = env && env.prod
+  return {
+    entry: './src/index.js',
+    devtool: dev && 'inline-source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'React Demo',
+      }),
     ],
-  },
-  mode: 'development',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: { loader: 'babel-loader' },
+        },
+      ],
+    },
+    mode: dev ? 'development' : prod ? 'production' : 'none',
+  }
 }
